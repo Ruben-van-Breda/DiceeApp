131aa1aa53a75b3d9074f81ef4107e0efa4b1621
RubenVB <re.vanbred@gmail.com>
2019-01-09 21:06:13 +0200

    Initial Commit

---
+++ Dicee App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee App
+//
+//  Created by Ruben van Breda on 1/9/19.
+//  Copyright © 2019 Ruben van Breda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
